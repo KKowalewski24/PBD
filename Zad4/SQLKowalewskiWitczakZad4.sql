f41618bc30069a0356140da007383bed1f94d53b
Kamil <szamil128@onet.pl>
2019-05-02 21:28:48 +0200

    Add required script + template of task

---
+++ Zad4/SQLKowalewskiWitczakZad4.sql
@@ -0,0 +1,89 @@
+-- UZYCIE BAZY TEST_PRACOWNICY --
+use test_pracownicy
+
+-- PODPUNKT 1 --
+
+
+-- PODPUNKT 2 --
+
+
+-- PODPUNKT 3 --
+
+
+-- PODPUNKT 4 --
+
+
+-- PODPUNKT 5 --
+
+
+-- PODPUNKT 6 --
+
+
+-- PODPUNKT 7 --
+
+
+-- PODPUNKT 8 --
+
+
+-- PODPUNKT 9 --
+
+
+-- PODPUNKT 10 --
+
+
+-- PODPUNKT 11 --
+
+
+-- PODPUNKT 12 --
+
+
+-- PODPUNKT 13 --
+
+
+-- PODPUNKT 14 --
+
+
+-- PODPUNKT 15 --
+
+
+-- PODPUNKT 16 --
+
+
+-- PODPUNKT 17 --
+
+
+-- PODPUNKT 18 --
+
+
+-- PODPUNKT 19 --
+
+
+-- PODPUNKT 20 --
+
+
+-- PODPUNKT 21 --
+
+
+-- PODPUNKT 22 --
+
+
+-- PODPUNKT 23 --
+
+
+-- PODPUNKT 24 --
+
+
+-- PODPUNKT 25 --
+
+
+
+
+
+
+
+
+
+
+
+
+
