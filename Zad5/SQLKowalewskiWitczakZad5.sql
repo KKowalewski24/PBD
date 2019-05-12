c980558e7e42c1d0d00bfd44cf5a3666a5bef908
Kamil <szamil128@onet.pl>
2019-05-12 20:26:32 +0200

    Add rest of points in task - whole done

--- Zad5/SQLKowalewskiWitczakZad5.sql
+++ Zad5/SQLKowalewskiWitczakZad5.sql
@@ -19,7 +19,7 @@ where last_name='Matos' or last_name='Taylor' order by hire_date, last_name
 select last_name, hire_date, dateadd(MONTH,6,hire_date) as kiedy from employees
 
 -- PODPUNKT 5 --
-select last_name, job_id, employees.department_id, department_name 
+select last_name, job_id, employees.department_id, department_name,city 
 from employees, departments, locations where city='Toronto' 
 
 -- PODPUNKT 6 --
@@ -33,19 +33,26 @@ select A.first_name, A.last_name, B.last_name as manager_name
 from employees A, employees B where B.employee_id=A.manager_id and B.last_name='King'
 
 -- PODPUNKT 8 --
-
+select A.last_name, A.salary,A.commission_pct 
+from employees A where A.commission_pct is not null
 
 -- PODPUNKT 9 --
-
+select (first_name+' '+last_name) "Pracownicy" from employees where last_name like '__a%' 
 
 -- PODPUNKT 10 --
-
+select first_name, last_name, employees.department_id, job_id, city
+from employees, departments, locations where city='Seattle'
 
 -- PODPUNKT 11 --
-
+select department_id, min(salary) as min_salary from employees 
+where department_id=(select top 1 department_id from employees group by department_id
+order by avg(salary) desc) group by department_id
 
 -- PODPUNKT 12 --
+select first_name, last_name, hire_date,datename(dw,hire_date) as weekday 
+from employees where datename(dw,hire_date)=(select top 1 datename(dw,hire_date) 
+from employees group by datename(dw, hire_date) order by count (*) desc)
 
-
--- PODPUNKT 12 --
+-- PODPUNKT 13 --
+select top 3 last_name, first_name, salary from employees order by salary desc
 
