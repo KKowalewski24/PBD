88f1c982f93b442b33559ae86483961267b5aa32
Kamil <szamil128@onet.pl>
2019-06-06 18:57:26 +0200

    Add new directory

---
+++ Extra/Extra1.sql
@@ -0,0 +1,58 @@
+use HR
+
+
+--PODPUNKT 1 --
+select A.last_name,B.job_title, A.salary from employees A, jobs B order by A.salary
+/*where B.job_title not like 's%' and  B.job_title not like '%d%' 
+and A.salary>2600 and A.salary<11000 order by A.salary*/
+
+--PODPUNKT 2 --
+
+--PODPUNKT 3 --
+
+--PODPUNKT 4 --
+
+--PODPUNKT 5 --
+
+--PODPUNKT 6 --
+
+--PODPUNKT 7 --
+
+--PODPUNKT 8 --
+
+--------------------------------------------------------------------------
+/*
+1. Wyswietl nazwisko,stanowisko oraz wysokosc pensji pracownika, ktorzy nie pracuja
+na stanowisku zaczynajacym sie na litere s ani na zadnym ze stanowisk posiadajacych 
+w swojej nazwie litere d oraz ktorych zarobki nie naleza do przedzialu od 2600 do 11000.
+Wyswietlane dane uporzadkuj rosnaco wedlug kolejnosci wysokosci pensji
+2. Ilu pracownikow podlega managerom posiadajacych identyfikatory z przedzialu:[100,107]
+a ilu nie przypisano manegara. Wyniki posortuj malejaco w zaleznosci od liczby pracownikow
+3.Ilu pracownikow zatrudniono w latach 1997-1999 na poszczegolnych stanowiskach nie 
+zawierajacych w nazwie slowa 'clerk'. Dane posortuj malejaco wedlug liczby zatrudnionych pracownikow
+4. Wypisz miasta z Azji, dla ktorych nie okreslono 'state_province'
+5.Podaj nazwy oddzialow oraz kwoty jakie kazdy z oddzialow wydaje na pensje swoich 
+pracownikow (wypisac 5 oddzialow, ktore wydaja najwiecej)
+6.Wyswietl nazwy krajow, miasta oraz liczbe departamentow w danym kraju i miescie. Zapytanie
+powinno uwzglednic tylko te panstwa i miasta, ktore posiadaja depertament
+7.Podaj nazwy panstw europejskich, w ktorych nie ma siedziby(locations) zaden oddzial
+8.Wyswietl tylko te oddzialy (department), ktore zatrudniaja pracownikow, ktorych zarobki nie
+przekraczaja 50% srednich zarobkow (wszystkich pracownikow)
+*/
+--------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
