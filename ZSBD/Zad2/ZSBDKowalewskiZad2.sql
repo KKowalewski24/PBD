3291671c9b7b56d794a082818f4c01d76bde5de6
Kamil Kowalewski <szamil128@onet.pl>
2019-09-26 19:03:13 +0200

    Add rest of task

--- ZSBD/Zad2/ZSBDKowalewskiZad2.sql
+++ ZSBD/Zad2/ZSBDKowalewskiZad2.sql
@@ -3,35 +3,25 @@
 USE biuro
 GO
 
-SELECT *
-FROM biura
+SELECT * FROM biura
 
-SELECT *
-FROM personel
+SELECT * FROM personel
 
-SELECT *
-FROM wlasciciele
+SELECT * FROM wlasciciele
 
-SELECT *
-FROM nieruchomosci
+SELECT * FROM nieruchomosci
 
-SELECT *
-FROM klienci
+SELECT * FROM klienci
 
-SELECT *
-FROM wynajecia
+SELECT * FROM wynajecia
 
-SELECT *
-FROM wizyty
+SELECT * FROM wizyty
 
-SELECT *
-FROM rejestracje
+SELECT * FROM rejestracje
 
-SELECT *
-FROM biura2
+SELECT * FROM biura2
 
-SELECT *
-FROM nieruchomosci2
+SELECT * FROM nieruchomosci2
 
 -- PODPUNKT 1 --
 SELECT nieruchomoscnr,
@@ -79,11 +69,7 @@ SELECT DISTINCT biuronr,
 FROM nieruchomosci nieru_zew
 
 -- PODPUNKT 5A --
-SELECT
-TOP 1
-miasto
-,
-count(*) AS suma
+SELECT TOP 1 miasto, count(*) AS suma
 FROM nieruchomosci, wynajecia
 WHERE nieruchomosci.nieruchomoscnr = wynajecia.nieruchomoscnr
 GROUP BY miasto
@@ -104,23 +90,93 @@ WHERE klienci.klientnr = wynajecia.klientnr
   AND wynajecia.nieruchomoscnr = wizyty.nieruchomoscnr
 
 -- PODPUNKT 7 --
-
+SELECT DISTINCT wynajecia.klientnr, count(DISTINCT wizyty.nieruchomoscnr)
+FROM wynajecia, wizyty
+WHERE wizyty.klientnr = wynajecia.klientnr
+  AND data_wizyty < od_kiedy
+  AND wizyty.nieruchomoscnr <> wynajecia.nieruchomoscnr -- OPERATOR <> MEANS NOT EQUAL TO
+GROUP BY wynajecia.klientnr
 
 -- PODPUNKT 8 --
-
+SELECT DISTINCT klienci.klientnr
+FROM klienci, wynajecia
+WHERE klienci.klientnr = wynajecia.klientnr
+  AND klienci.max_czynsz < wynajecia.czynsz
 
 -- PODPUNKT 9 --
+SELECT biuronr
+FROM biura
+WHERE biura.biuronr NOT IN (SELECT biuronr FROM nieruchomosci)
 
+-- WERSJA Z LEFT JOIN, NIE USUWAC
+-- SELECT biu.biuronr
+-- FROM biura biu
+--          LEFT JOIN nieruchomosci nieru ON biu.biuronr = nieru.biuronr
+-- WHERE nieru.biuronr IS NULL
 
 -- PODPUNKT 10A --
-
+SELECT (
+           SELECT count(*)
+           FROM personel
+           WHERE plec = 'k'
+       ) AS kobiety,
+       (
+           SELECT count(*)
+           FROM personel
+           WHERE plec = 'M'
+       ) AS mezczyzni
 
 -- PODPUNKT 10B --
-
+SELECT DISTINCT biu.biuronr,
+                (
+                    SELECT count(*)
+                    FROM personel per
+                    WHERE plec = 'k'
+                      AND biu.biuronr = biuronr
+                ) AS kobiety,
+                (
+                    SELECT count(*)
+                    FROM personel per
+                    WHERE plec = 'M'
+                      AND biu.biuronr = biuronr
+                ) AS mezczyzni
+FROM biura biu, personel
+WHERE biu.biuronr = personel.biuronr
 
 -- PODPUNKT 10C --
-
+SELECT DISTINCT miasto,
+                (
+                    SELECT count(*)
+                    FROM biura, personel
+                    WHERE plec = 'k'
+                      AND biura.biuronr = personel.biuronr
+                      AND biura.miasto = biu.miasto
+                ) AS kobiety,
+                (
+                    SELECT count(*)
+                    FROM biura, personel
+                    WHERE plec = 'M'
+                      AND biura.biuronr = personel.biuronr
+                      AND biura.miasto = biu.miasto
+                ) AS mezczyzni
+FROM biura biu, personel
+WHERE biu.biuronr = personel.biuronr
 
 -- PODPUNKT 10D --
-
-
+SELECT DISTINCT stanowisko,
+                (
+                    SELECT count(*)
+                    FROM biura, personel
+                    WHERE plec = 'k'
+                      AND biura.biuronr = personel.biuronr
+                      AND personel.stanowisko = per.stanowisko
+                ) AS kobiety,
+                (
+                    SELECT count(*)
+                    FROM biura, personel
+                    WHERE plec = 'M'
+                      AND biura.biuronr = personel.biuronr
+                      AND personel.stanowisko = per.stanowisko
+                ) AS mezczyzni
+FROM biura biu, personel per
+WHERE biu.biuronr = per.biuronr
