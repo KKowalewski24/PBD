07133c0a899534b5bb879bbdcd278b1146b4610b
Kamil Kowalewski <44941204+KKowalewski24@users.noreply.github.com>
2019-10-08 19:44:00 +0200

    Add comments to 4th task, #3

--- ZSBD/Zad4/ZSBDKowalewskiZad4.sql
+++ ZSBD/Zad4/ZSBDKowalewskiZad4.sql
@@ -1,14 +1,14 @@
 -- KAMIL KOWALEWSKI 216806 --
 
--- PODPUNKT 1 --
+-- PODPUNKT 1. Utwórz blok wypisujący na ekranie "Część, to ja". --
 DECLARE @greeting VARCHAR(25) ='Czesc, to ja'
 PRINT @greeting
 
--- PODPUNKT 2 --
+-- PODPUNKT 2. Utwórz blok, który zawiera: deklaracje zmiennej numerycznej, przypisanie jej wartości oraz wypisanie jej na ekranie w postaci ZMIENNA = ....... --
 DECLARE @number INT=5
 PRINT 'Zmienna = ' + convert(VARCHAR(25), @number)
 
--- PODPUNKT 3 --
+-- PODPUNKT 3. Utwórz blok z zadeklarowaną zmienną wykorzystujący złożoną postać wyrażenia warunkowego (IF ... ELSE ...) i wypisujący rezultat na ekranie. --
 DECLARE @num_value INT=15
 
 IF (@num_value = 15)
@@ -16,7 +16,7 @@ IF (@num_value = 15)
 ELSE
     PRINT 'wartosc zmiennej jest rozna od 15'
 
--- PODPUNKT 4 --
+-- PODPUNKT 4. Wykorzystując pętle, utwórz blok anonimowy wypisujący. --
 DECLARE @iterator_1 INT=1
 
 WHILE (@iterator_1 < 5)
@@ -25,7 +25,7 @@ WHILE (@iterator_1 < 5)
         SET @iterator_1 = @iterator_1 + 1
     END;
 
--- PODPUNKT 5 --
+-- PODPUNKT 5. Napisz blok wykonujący pętle od wartości licznika 3 do 7, wypisujący na ekranie kolejne wartości licznika oraz komentarze: "początek" dla wartości 3, "środek" dla 5 i "koniec" dla 7. --
 DECLARE @iterator_2 INT=3
 
 WHILE (@iterator_2 <= 7)
@@ -43,7 +43,7 @@ WHILE (@iterator_2 <= 7)
         SET @iterator_2 = @iterator_2 + 1
     END;
 
--- PODPUNKT 6 --
+-- PODPUNKT 6. Utwórz w testowej bazie danych tabelę ODDZIALY o dwóch polach: NR_ODD (INT) i NAZWA_ODD (varchar(30)). --
 IF EXISTS(SELECT 1
           FROM master.dbo.sysdatabases
           WHERE name = 'sample_datebase')
@@ -59,7 +59,7 @@ CREATE TABLE sample_datebase.dbo.oddzialy (
 )
 GO
 
--- PODPUNKT 7 --
+-- PODPUNKT 7. Utwórz blok wypisujący na ekranie nazwę wybranego oddziału (np. o numerze 1 - KSIEGOWOSC) w postaci: Nazwa oddziału to: .............. --
 INSERT INTO sample_datebase.dbo.oddzialy
 VALUES (1, 'Ksiegowosc')
 
@@ -70,7 +70,7 @@ BEGIN
     PRINT @nazwa
 END;
 
--- PODPUNKT 8 --
+-- PODPUNKT 8. Utwórz blok anonimowy, który poprzez kursor pozwoli na wypisanie w pętli numerów i nazw oddziałów. --
 DECLARE @iterator_3 INT=0, @nazwa_3 VARCHAR(40)
 
 WHILE exists(SELECT *
@@ -88,7 +88,7 @@ WHILE exists(SELECT *
             END;
     END;
 
--- PODPUNKT 9 --
+-- PODPUNKT 9. Z pomocą kursora usuń z tabeli ODDZIALY te rekordy, których numer jest większy od dwóch (przy założeniu, że oddziały ponumerowane są kolejno i jest ich więcej niż 2), i następnie, jeśli usunął jakieś oddziały, to wypisuje ich liczbę (Liczba usunietych rekordów to: .....) --
 DECLARE @iterator_4 INT=2, @numer_4 INT=2
 BEGIN
     WHILE exists(SELECT * FROM sample_datebase.dbo.oddzialy WHERE nr_odd > @numer_4)
@@ -99,7 +99,7 @@ BEGIN
     PRINT 'Liczba usunietych rekordow to: ' + convert(VARCHAR(10), (@iterator_4 - @numer_4))
 END;
 
--- PODPUNKT 10 --
+-- PODPUNKT 10. Utwórz blok anonimowy, który poprzez kursor zmienia nazwę oddziału o numerze 3, a jeśli taki numer nie istnieje, to dodaje nowy wiersz do tabeli o tym numerze oddziału i nazwie. Sprawdzić działanie bloku w obu przypadkach. --
 DECLARE @numer_5 INT=3
 
 BEGIN
