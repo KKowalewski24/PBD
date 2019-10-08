0d84e2de5e9804672362d9e6fb1748179b98a169
Kamil Kowalewski <44941204+KKowalewski24@users.noreply.github.com>
2019-10-08 20:13:27 +0200

    Add comments to 5th task

--- ZSBD/Zad5/ZSBDKowalewskiZad5.sql
+++ ZSBD/Zad5/ZSBDKowalewskiZad5.sql
@@ -18,7 +18,7 @@ FROM test_pracownicy.dbo.pracownicy;
 SELECT *
 FROM test_pracownicy.dbo.prac_archiw;
 
--- PODPUNKT 1 --
+-- PODPUNKT 1. Utwórz tabele dziennik składająca się z pół: tabela (do piętnastu znaków), data, l_wierszy (liczba całkowita), komunikat (do trzystu znaków). --
 CREATE TABLE test_pracownicy.dbo.dziennik (
     tabela    VARCHAR(15)  NOT NULL,
     data      DATE         NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE test_pracownicy.dbo.dziennik (
     komunikat VARCHAR(300) NOT NULL,
 );
 
--- PODPUNKT 2 --
+-- PODPUNKT 2. Zadeklaruj blok anonimowy aktualizujący tabele pracownicy poprzez dodanie premii tym pracownikom, którzy są kierownikami. Wartość premii jest ustawiana przez zmienną zadeklarowana w bloku. Następnie policz ile zmieniono wierszy i wstaw liczbę zmian oraz odpowiedni komentarz do tabeli dziennik (Wprowadzono dodatek funkcyjny w wysokości ...). --
 SELECT nr_akt, placa
 FROM test_pracownicy.dbo.pracownicy
 WHERE nr_akt IN (
@@ -70,7 +70,7 @@ SELECT *
 FROM test_pracownicy.dbo.dziennik;
 GO
 
--- PODPUNKT 3 --
+-- PODPUNKT 3. Zadeklaruj blok wstawiający do dziennika komentarz o ilości zatrudnionych pracowników w wybranym roku (np. 1989) (Zatrudniono ... pracowników./Nikogo nie zatrudniono). --
 DECLARE @rok INT=1989, @liczba_prac INT
 BEGIN
     SET @liczba_prac = (
@@ -93,7 +93,7 @@ SELECT *
 FROM test_pracownicy.dbo.dziennik;
 GO
 
--- PODPUNKT 4 --
+-- PODPUNKT 4. Zadeklaruj blok anonimowy wstawiający do dziennika komentarz o długości zatrudnienia pracownika z numerem 8902 (dłużej, czy krócej niż 15 lat): Pracownik ... jest zatrudniony [dłużej niż / krócej niż] 15 lat. --
 DECLARE @numer INT=8902
 BEGIN
     IF (year(getdate()) -
@@ -117,7 +117,7 @@ SELECT *
 FROM test_pracownicy.dbo.dziennik;
 GO
 
--- PODPUNKT 5 --
+-- PODPUNKT 5. Utwórz procedurę składowana PIERWSZA, która wyświetli wartości pobranego argumentu w postaci: wartość parametru wynosiła: ...... Wywołaj procedurę z bloku. --
 
 -- drop PROCEDURE pierwsza
 CREATE PROCEDURE pierwsza(@parametr INT) AS
@@ -129,7 +129,7 @@ GO
 EXEC pierwsza 15
 GO
 
--- PODPUNKT 6 --
+-- PODPUNKT 6. Utwórz procedurę DRUGA o następujących własnościach: trzy argumenty: wejściowy ciąg znaków domyślnie NULL, wyjściowy ciąg znaków oraz wejściowy numer z przypisana wartością początkowa 1; Zadeklarowana zmienna lokalna znakowa niezerowa z przypisana wartością 'DRUGA'; Ciąg zwracany to łańcuch składający się z wartości zmiennej lokalnej, ciągu wejściowego oraz numeru wejściowego. --
 CREATE PROCEDURE druga(@wejscie VARCHAR(100) NULL, @wyjscie VARCHAR(100) OUTPUT, @liczba INT=1) AS
 BEGIN
     DECLARE @zmienna VARCHAR(10)='nazwa'
@@ -142,7 +142,7 @@ EXEC druga 'Napis', @wyjscie OUTPUT, 6
 SELECT @wyjscie
 GO
 
--- PODPUNKT 7 --
+-- PODPUNKT 7. Utwórz procedurę, podwyższająca płace dla danego argumentem działu określony drugim argumentem procent. Wprowadź domyślne wartości dla argumentów, a także odpowiedni komentarz do dziennika. Policz zmodyfikowane rekordy. (Zaktualizuj atrybut płaca, wstaw komunikat do dziennika w postaci: Wprowadzono podwyżkę o ... procent). Jeśli numer działu to zero, podnieś płace wszystkim pracownikom. --
 
 -- TODO, ISSUE WITH CONVERTING MONEY TO INT
 -- DROP PROCEDURE zmien_place
@@ -179,7 +179,7 @@ SELECT *
 FROM test_pracownicy.dbo.dziennik
 GO
 
--- PODPUNKT 8 --
+-- PODPUNKT 8. Napisz funkcje zwracająca udział procentowy działu w budżecie firmy. Wykonaj ja wewnątrz zapytania dającego wynik w postaci dwóch kolumn: id_dzialu, udzial_w_budzecie. --
 
 -- drop FUNCTION procentowy_budzet_firmy
 CREATE FUNCTION procentowy_budzet_firmy(@numer_dzialu INT) RETURNS DECIMAL(10, 2)
@@ -199,7 +199,7 @@ SELECT DISTINCT id_dzialu, dbo.procentowy_budzet_firmy(id_dzialu)
 FROM test_pracownicy.dbo.pracownicy
 GO
 
--- PODPUNKT 9 --
+-- PODPUNKT 9. Utwórz wyzwalacz do_archiwum, który przenosi dane pracownika do tabeli prac_archiw w przypadku jego zwolnienia (usunięcia z tabeli pracownicy). Dodaj komentarz do tablicy dziennik: Zwolniono pracownika numer: ... --
 CREATE TRIGGER do_archiwum
     ON test_pracownicy.dbo.pracownicy
     FOR DELETE AS
