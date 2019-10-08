33e2480893fbe83eaf97a4d29e03ce022b7861df
Kamil Kowalewski <44941204+KKowalewski24@users.noreply.github.com>
2019-10-08 20:18:51 +0200

    Add comments to 6th task

--- ZSBD/Zad6/ZSBDKowalewskiZad6.sql
+++ ZSBD/Zad6/ZSBDKowalewskiZad6.sql
@@ -3,7 +3,7 @@
 USE biuro
 GO
 
--- PODPUNKT 1 --
+-- PODPUNKT 1. Napisz procedurę, która: dodaje nowego wlasciciela; wywoływana jest z 4 parametrami: imie, nazwisko, adres, telefon; numer wlasciciela bedzie generowany automatycznie (nalezy sprawdzic numery w tabeli; wybrac brakujacy i dopisac przedrostek); Napisz wywolanie procedury --
 
 -- DROP PROCEDURE dodaj_wlasciciela
 CREATE PROCEDURE dodaj_wlasciciela(@imie VARCHAR(15), @nazwisko VARCHAR(25),
@@ -28,7 +28,7 @@ SELECT *
 FROM biuro.dbo.wlasciciele;
 GO
 
--- PODPUNKT 2 --
+-- PODPUNKT 2. Napisz funkcję, która dla kaŜdego biura poda jego przychody z wynajmu. --
 CREATE FUNCTION podaj_przychody_wynajmu()
     RETURNS @udzial TABLE (
         biuronr VARCHAR(4),
@@ -47,7 +47,7 @@ SELECT *
 FROM podaj_przychody_wynajmu();
 GO
 
--- PODPUNKT 3 --
+-- PODPUNKT 3. Napisz wyzwalacz, który podczas wstawiania nowego rekordu do tabeli wynajecia, w przypadku, gdy podany czynsz jest wyzszy od maksymalnego podanego przez klienta: wprowadzi maksymalny czysz dla tego klienta; wypisze stosowny komunikat --
 
 -- DROP TRIGGER czynsz_wynajecia
 CREATE TRIGGER czynsz_wynajecia
@@ -77,7 +77,7 @@ FROM biuro.dbo.wynajecia
 WHERE umowanr = 1118;
 GO
 
--- PODPUNKT 4 --
+-- PODPUNKT 4. Napisz wyzwalacz, który przy wprowadzeniu nowego klienta, dodaje dla niego rejestrację z wybranym numerem pracownika. --
 
 -- DROP TRIGGER wprowadz_nowego_klienta
 CREATE TRIGGER wprowadz_nowego_klienta
@@ -106,7 +106,7 @@ VALUES ('CO20', 'Kamil', 'Kowalewski', 'Wolczanska 21, 91-237 Lodz',
 SELECT *
 FROM biuro.dbo.rejestracje;
 
--- PODPUNKT 5 --
+-- PODPUNKT 5. Napisz funkcję obliczającą prowizję dla każdego pracownika przyjmując następujące założenia: prowizja liczna jest w zakresie podanym przez uzytkownika (data_od data_do); za wizytę pracownik otrzymuje 2% pensji, za wynajęcie 10% --
 
 -- DROP FUNCTION oblicz_prowizje
 CREATE FUNCTION oblicz_prowizje(@data_od DATETIME, @data_do DATETIME)
@@ -138,7 +138,7 @@ SELECT *
 FROM oblicz_prowizje('1999 - 01 - 01', '2004 - 08 - 24');
 GO
 
--- PODPUNKT 6 --
+-- PODPUNKT 6. Napisz pracedurę, która wypisuje wszystkie niezaplacone nieruchomosci w postaci: Brak wplaty od .... (nazwisko najemcy) za nieruchomosc nr .... za okres .... miesiecy. Uporzadkuj je od najstarszej niezaplaconej. --
 
 -- DROP PROCEDURE niezaplacone_nieruchomosci
 CREATE PROCEDURE niezaplacone_nieruchomosci AS
